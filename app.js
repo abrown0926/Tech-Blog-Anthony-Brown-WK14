cf8014d9cfc585997ba83b374ff0ae5ed38de4a1
AQB <abrown0926>
2021-11-02 12:43:59 -0500

    third commit added route folders

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const sequelize = require("./config/connection");
 
-const app = express();
+const app = new express();
 
 app.use(express.static("public"));
 
@@ -10,12 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
+// Set Handlebars as the default template engine.
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
 });
 
 app.get("/post", (req, res) => {
